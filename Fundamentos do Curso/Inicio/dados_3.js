7cc446d146b85a9c94ed0093e1d760025a6ea6e5
Yago Mendes <ygmendes21@gmail.com>
2023-05-24 00:14:26 -0300

    Update dados_3.js

---
+++ Fundamentos do Curso/Inicio/dados_3.js
@@ -0,0 +1,12 @@
+let idade = 20;
+console.log(typeof 20);
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log (typeof salario);
+
+let estaChovendo = true // ou false
+console.log(typeof estaChovendo);
+
+console.log(typeof "Teste"); // String sequencia de caracteres
+console.log(typeof 'Teste');
