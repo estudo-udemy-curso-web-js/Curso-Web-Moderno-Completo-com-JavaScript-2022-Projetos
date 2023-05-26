d00a911a056b11e0c1a7a587a37150026d11fac2
Yago Mendes <ygmendes21@gmail.com>
2023-05-25 23:54:31 -0300

    Create comentario.js

---
+++ Fundamentos do Curso/Exercicios/Fundamentos/comentario.js
@@ -0,0 +1,14 @@
+// Comentário de uma linha
+console.log('Linha 1');
+
+/*
+Comentario de
+multiplas linhas
+*/
+console.log('Linha 2');
+
+/*
+*Comentario de
+*multiplas linhas
+*/
+console.log('Linha 3');
